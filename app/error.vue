ea86cfe5ab8666080b940b95242e8acdbfd41b31
puranchandra <puran@exlitem.com>
2025-08-16 01:00:41 +0530

    Update nuxt.config.ts to improve SSR settings and refine prerender routes for better deployment on GitHub Pages

---
+++ app/error.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Error</h1>
+        <Button type="button" variant="default" @click="navigateTo('/')">Go to home</Button>
+    </div>
+</template>
+
+<script setup>
+import { Button } from '@/components/ui/button';
+</script>
